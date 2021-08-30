2f0f1c2e18cbcdc445e923a0150e2f357861090e
Wambui Gitau <ywambui@heptanalytics.com>
2021-08-30 22:43:57 +0200

    made changes to featurization

--- lessons/utils/featurization.py
+++ lessons/utils/featurization.py
@@ -7,11 +7,12 @@ class Bow:
         # the words will be store here
         self.all_word = set()
         #map words to index and reverse
-        self.word_to_index = []
-        self.index_to_word = []
+        self.word_to_index = {}
+        self.index_to_word = {}
 
 
     def fit(self,document):
+        
         #check if its a list and its not empty
         if type(document) != list or len(document) < 1 or type(document[0]) != str:
             raise TypeError ('You must pass a lis of strings')
@@ -29,20 +30,22 @@ class Bow:
 
     def transform(self,data):
         if len(self.all_word) == 0:
-            raise AttributeError('Tou must fit the data first')
+            raise AttributeError('You must fit the data first')
 
-        # if tou have one sentence
+        # if you have one sentence
         if type(data) == str:
             transformed = self._transform_single(data.split())
         
         #if you have a list of sentences
         elif data == list or type(data[0]) == str:
             #create a empty matrix
-            transformed = np.empty(len(data),len(self.all_word))
+            
+            transformed = np.empty((len(data),len(self.all_word)))
 
             for row,sentence in enumerate(data):
                 #replace row with sentence BoW
-                transformed[row] = self._transform_single(data.split())
+                
+                transformed[row] = self._transform_single(sentence.split())
         else:
             raise TypeError("You must pass either list or string")
 
@@ -80,6 +83,7 @@ class TDIDF:
         self.index_to_word = {}
         self.idf = {}
         self.num_documents = 0
+        
 
     def term_frequency(self,sentence,ignore_tokens=['<SOS>',"<EOS>"],lower_case=False):
         word_dict ={}
@@ -114,14 +118,14 @@ class TDIDF:
         document_words = list(global_term_freq.keys())
         for word_position in range(len(document_words)):
             word = document_words[word_position]
-            self.word_indexes[word] = word_position
+            self.word_to_index[word] = word_position
             self.index_to_word[word_position] = word
 
     def transform(self, data):
         if isinstance(data,list):
             return self._transform_document(data)
         elif isinstance(data,str):
-            return self._transform_sentence(data)
+            return self._tranform_sentence(data)
 
     def _transform_document(self,data):
         #used for batch processing
@@ -129,7 +133,7 @@ class TDIDF:
         sentence_arrays = []
 
         for sent in data:
-            sentence_arrays.append(self._transform_sentence(sent))
+            sentence_arrays.append(self._tranform_sentence(sent))
 
         return np.matrix(sentence_arrays)
 
@@ -137,7 +141,7 @@ class TDIDF:
         tokens = [token.lower() if self.lower_case else token for token in data.split()]
 
         word_array = np.zeros(len(self.word_to_index))
-        sent_td_idf = self._compute_sent_td_idf(data)
+        sent_td_idf = self._compute_sentence_tf_idf(data)
 
         for token in tokens:
             if token in self.word_to_index:
@@ -157,6 +161,7 @@ class TDIDF:
         total_words = sum(document_frequency.values())
         # Find individual term frequency value averaged by total number of words.
         averaged_frequency = {k:(float(v)/total_words) for k,v in document_frequency.items()}
+        
         for term, tf in averaged_frequency.items():
             # Out of vocabulary words are simply zeroed. They are going to be removed later either way.
             # Computes the tfidf for each word by using word tf times the term idf
